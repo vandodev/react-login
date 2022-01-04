1fbe2c8e6416b1ecb0e88cd6ca15f1f9ea3c45c3
Evandro oliveira <vandoaparecido@hotmail.com>
2022-01-04 02:02:28 -0300

    :closed_lock_with_key: Estrutura do projeto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <div className="app">
+      <LoginPage />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
