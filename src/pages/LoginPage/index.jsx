5bee24ab1b5d09b717c4d39d1a72d13054b13f61
Evandro oliveira <vandoaparecido@hotmail.com>
2022-01-04 02:46:25 -0300

    :closed_lock_with_key: Capturando valores dos campos e mostrando no console

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./styles.css";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSunmit = (e) => {
+    e.preventDefault();
+    console.log("submit", { email, password });
+  };
   return (
     <div id="login">
       <h1 className="title">Login do sistema</h1>
-      <form className="form">
+      <form className="form" onSubmit={handleSunmit}>
         <div className="field">
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            id="email"
+          />
         </div>
 
         <div className="field">
           <label htmlFor="password">Senha</label>
-          <input type="password" name="password" id="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+            id="password"
+          />
         </div>
 
         <div className="action">
