ee4f1d401e88aa5418ad33e2c4578f4f65380a9a
Evandro oliveira <vandoaparecido@hotmail.com>
2022-01-06 01:01:46 -0300

    :closed_lock_with_key: Trabalhando com useContext e regras de login

--- src/AppRouters.jsx
+++ src/AppRouters.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -8,13 +9,28 @@ import {
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 
+import { AuthContext } from "./contexts/auth";
+
 const AppRoutes = () => {
+  const [user, setUser] = useState(null);
+
+  const login = (email, password) => {
+    console.log("Login", { email, password });
+    setUser({ id: 123, email });
+  };
+  const logout = () => {
+    console.log("logout");
+  };
   return (
     <Router>
-      <Routes>
-        <Route exact path="/login" element={<LoginPage />} />
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <AuthContext.Provider
+        value={{ authentecated: !!user, user, login, logout }}
+      >
+        <Routes>
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </AuthContext.Provider>
     </Router>
   );
 };
--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,17 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { AuthContext } from "../../contexts/auth";
 
 import "./styles.css";
 
 const LoginPage = () => {
+  const { authentecated, login } = useContext(AuthContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
   const handleSunmit = (e) => {
     e.preventDefault();
     console.log("submit", { email, password });
+    login(email, password);
   };
   return (
     <div id="login">
       <h1 className="title">Login do sistema</h1>
+      <p>{String(authentecated)}</p>
       <form className="form" onSubmit={handleSunmit}>
         <div className="field">
           <label htmlFor="email">Email</label>
