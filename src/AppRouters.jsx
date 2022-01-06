426989202abf4b4306ae80539489265d97925d10
Evandro oliveira <vandoaparecido@hotmail.com>
2022-01-06 01:32:52 -0300

    :closed_lock_with_key: Refatorando provider

--- src/AppRouters.jsx
+++ src/AppRouters.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -9,28 +9,17 @@ import {
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 
-import { AuthContext } from "./contexts/auth";
+import { AuthProvider } from "./contexts/auth";
 
 const AppRoutes = () => {
-  const [user, setUser] = useState(null);
-
-  const login = (email, password) => {
-    console.log("Login", { email, password });
-    setUser({ id: 123, email });
-  };
-  const logout = () => {
-    console.log("logout");
-  };
   return (
     <Router>
-      <AuthContext.Provider
-        value={{ authentecated: !!user, user, login, logout }}
-      >
+      <AuthProvider>
         <Routes>
           <Route exact path="/login" element={<LoginPage />} />
           <Route path="/" element={<HomePage />} />
         </Routes>
-      </AuthContext.Provider>
+      </AuthProvider>
     </Router>
   );
 };
--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -1,3 +1,22 @@
-import React, { createContext } from "react";
+import React, { useState, createContext } from "react";
 
 export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const login = (email, password) => {
+    console.log("Login", { email, password });
+    setUser({ id: 123, email });
+  };
+  const logout = () => {
+    console.log("logout");
+  };
+  return (
+    <AuthContext.Provider
+      value={{ authentecated: !!user, user, login, logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
